dc6abeaf2105d758c9e78feac598983733351101
Andrew Fillenwarth <andyf016@gmail.com>
2020-01-15 12:31:26 -0500

    convert to class style syntax

---
+++ script.js
@@ -0,0 +1,128 @@
+class Traveler{
+    constructor(name){
+        this.name = name;
+        this.food = 1;
+        this.ishealthy = true;
+    }
+    hunt (){
+        this.food += 2;
+    }
+
+    eat (){
+        if(this.food === 0){
+            this.ishealthy = false
+        } else{
+            this.food -=1;
+        }
+    }
+}
+
+class Wagon{
+    constructor(capacity){
+        this.capacity = capacity;
+        this.passengers = [];
+    }
+    getAvailableSeatCount (){
+        let count = this.capacity - this.passengers.length;
+        return count;
+    }
+    join (traveler){
+        if(this.passengers.length >= this.capacity){
+            
+        }else
+            this.passengers.push(traveler);
+    }
+    shouldQuarantine (){
+        for(let i = 0; i < this.passengers.length; i++){
+            if(this.passengers[i].ishealthy === false){
+                return true;
+            }
+                
+        }return false;
+    }
+    totalFood (){
+        let eats = 0 
+        for(let i = 0; i < this.passengers.length; i++){
+            eats = this.passengers[i].food + eats
+        } return eats
+    }
+}
+
+
+
+
+class Doctor extends Traveler{ 
+    constructor (name, food, ishealthy){
+    super(name, food, ishealthy)
+    }
+
+    heal (Traveler){
+        if(Traveler.ishealthy == false){
+            Traveler.ishealthy = true
+        }
+    }
+}
+
+
+class Hunter extends Traveler{ 
+    constructor (name, food, ishealthy, numOfFoodUnits){
+        super(name, food, ishealthy)
+        this.numOfFoodUnits = numOfFoodUnits;
+        this.food = 2 ;
+    }
+    hunt (){
+        this.food +=5
+        this.ishealthy = true
+    }
+    eat (){
+        if(this.food >=2){
+            this.food -= 2;
+        }else if (this.food % 2 == 1){
+            this.food -= 1 
+            this.ishealthy = false
+        }
+    }
+    giveFood (Traveler, numOfFoodUnits){
+        if(this.food > numOfFoodUnits){
+            this.food -= numOfFoodUnits;
+            Traveler.food += numOfFoodUnits;
+        } else if (this.food < numOfFoodUnits){
+            return;
+        }
+    }
+}
+
+
+// Create a wagon that can hold 4 people
+let wagon = new Wagon(4);
+// Create five travelers
+let henrietta = new Traveler('Henrietta');
+let juan = new Traveler('Juan');
+let drsmith = new Doctor('Dr. Smith');
+let sarahunter = new Hunter('Sara');
+let maude = new Traveler('Maude');
+console.log(`#1: There should be 4 available seats. Actual: ${wagon.getAvailableSeatCount()}`);
+wagon.join(henrietta);
+console.log(`#2: There should be 3 available seats. Actual: ${wagon.getAvailableSeatCount()}`);
+wagon.join(juan);
+wagon.join(drsmith);
+wagon.join(sarahunter);
+wagon.join(maude); // There isn't room for her!
+console.log(`#3: There should be 0 available seats. Actual: ${wagon.getAvailableSeatCount()}`);
+console.log(`#4: There should be 5 total food. Actual: ${wagon.totalFood()}`);
+sarahunter.hunt(); // gets 5 more food
+drsmith.hunt();
+console.log(`#5: There should be 12 total food. Actual: ${wagon.totalFood()}`);
+henrietta.eat();
+sarahunter.eat();
+drsmith.eat();
+juan.eat();
+juan.eat(); // juan is now hungry (sick)
+console.log(`#6: Quarantine should be true. Actual: ${wagon.shouldQuarantine()}`);
+console.log(`#7: There should be 7 total food. Actual: ${wagon.totalFood()}`);
+drsmith.heal(juan);
+console.log(`#8: Quarantine should be false. Actual: ${wagon.shouldQuarantine()}`);
+sarahunter.giveFood(juan, 4);
+sarahunter.eat(); // She only has 1, so she eats it and is now sick
+console.log(`#9: Quarantine should be true. Actual: ${wagon.shouldQuarantine()}`);
+console.log(`#10: There should be 6 total food. Actual: ${wagon.totalFood()}`);
